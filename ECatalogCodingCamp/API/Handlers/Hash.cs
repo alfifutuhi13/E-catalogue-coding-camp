9ae46dad38225d61eab675871d9e34b0aaaa469b
alfifutuhi13 <alfi.futuhi13@gmail.com>
2021-05-26 20:44:45 +0700

    Add Handlers: Hash and SendEmail

---
+++ ECatalogCodingCamp/API/Handlers/Hash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Handlers
+{
+    public class Hash
+    {
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool ValidatePassword(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
+}
---
+++ ECatalogCodingCamp/API/Handlers/SendEmail.cs
@@ -0,0 +1,41 @@
+﻿using API.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace API.Handlers
+{
+    public class SendEmail
+    {
+        private readonly MyContext context;
+        public SendEmail(MyContext context)
+        {
+            this.context = context;
+        }
+
+        public void SendForgotPassword(string token, string resetCode, string email)
+        {
+            //[FromBody]Parameter parameter
+            var parameter = context.Parameters.Find(1);
+            var time24 = DateTime.Now.ToString("HH:mm:ss");
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.EnableSsl = true;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Credentials = new NetworkCredential(parameter.Name, parameter.Value);
+
+            MailMessage m = new MailMessage();
+            m.From = new MailAddress(parameter.Name);
+            m.To.Add(new MailAddress(email));
+            m.Subject = "Reset Password " + time24;
+            m.IsBodyHtml = false;
+            m.Body = "Hi " + "\nThis is your reset code for your account. "
+                + resetCode + "\nReset link: https://localhost:44320/api/account/resetpassword \nToken: " + token + "\n Thank You";
+            smtp.Send(m);
+        }
+    }
+}

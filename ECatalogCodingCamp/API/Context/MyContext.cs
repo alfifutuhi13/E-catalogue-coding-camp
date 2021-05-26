fc63dabcf47bdabe24721ef0a46e45148e55fe9b
alfifutuhi13 <alfi.futuhi13@gmail.com>
2021-05-26 10:57:11 +0700

    Update MyContext.cs

--- ECatalogCodingCamp/API/Context/MyContext.cs
+++ ECatalogCodingCamp/API/Context/MyContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,26 @@ namespace API.Context
 
         public MyContext(DbContextOptions<MyContext> options) : base(options)
         { }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<CV> CVs { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<InterviewRequest> InterviewRequests { get; set; }
+        public DbSet<JobRole> JobRoles { get; set; }
+        public DbSet<Major> Majors { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<Parameter> Parameters { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<University> Universities { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        //public override void OnModelCreating(ModelBuilder modelBuilder) 
+        //{ 
+        //}
+
     }
 }

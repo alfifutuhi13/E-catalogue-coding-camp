dd66a15e9749c01f5eb0edcf7dc850ae43fcc97d
alfifutuhi13 <alfi.futuhi13@gmail.com>
2021-05-26 10:51:34 +0700

    Creating My context (-)ModelBuilder

---
+++ ECatalogCodingCamp/API/Context/MyContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext()
+        {}
+
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        { }
+    }
+}

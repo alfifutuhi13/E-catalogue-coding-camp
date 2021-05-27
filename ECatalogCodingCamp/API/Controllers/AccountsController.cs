60f1a4418bb9238d741b0037dbd8eb4e3f52b085
Arsya Baharin <alfatehanarsya8998@gmail.com>
2021-05-27 14:58:06 +0700

    Login Controller

--- ECatalogCodingCamp/API/Controllers/AccountsController.cs
+++ ECatalogCodingCamp/API/Controllers/AccountsController.cs
@@ -1,10 +1,17 @@
 ﻿using API.Base;
+using API.Context;
+using API.Handlers;
 using API.Models;
 using API.Repositories.Data;
+using API.Repositories.Interface;
+using API.ViewModels;
+using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,10 +21,48 @@ namespace API.Controllers
     [ApiController]
     public class AccountsController : BaseController<Account, AccountRepository, int>
     {
-        private readonly AccountRepository accountRepository;
-        public AccountsController(AccountRepository accountRepository) : base(accountRepository)
+        private SendEmail sendEmail;
+        private AccountRepository accountRepository;
+        private MyContext context;
+        private IConfiguration _config;
+        private readonly IGenericDapper _dapper;
+
+        public AccountsController(AccountRepository accountRepository, MyContext context, IConfiguration _config, SendEmail sendEmail, IGenericDapper dapperr) : base(accountRepository)
         {
             this.accountRepository = accountRepository;
+            this.context = context;
+            this._config = _config;
+            this.sendEmail = sendEmail;
+            _dapper = dapperr;
+        }
+
+        [HttpPost("Login/")]
+        public ActionResult Login(LoginVM login)
+        {
+            try
+            {
+
+                var dbparams = new DynamicParameters();
+                dbparams.Add("Email", login.Email, DbType.String);
+                dynamic result = _dapper.Get<dynamic>("[dbo].[SP_Login]"
+                , dbparams,
+                commandType: CommandType.StoredProcedure);
+
+                if (Hash.ValidatePassword(login.Password, result.Password))
+                {
+                    var jwt = new JwtService(_config);
+                    var token = jwt.GenerateSecurityLoginToken(result.Name, result.Email, result.Role);
+                    return Ok(new { token });
+                }
+
+                return Unauthorized("Failed To Make Token, Email / Password Wrong");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException);
+
+            }
+
         }
     }
 }

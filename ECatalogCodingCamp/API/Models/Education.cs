8490d92191827f6f88ba28ed68c33fd5f5ef6c28
alfifutuhi13 <alfi.futuhi13@gmail.com>
2021-05-27 07:53:23 +0700

    Update Key

--- ECatalogCodingCamp/API/Models/Education.cs
+++ ECatalogCodingCamp/API/Models/Education.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace API.Models
     [Table("TB_T_Education")]
     public class Education
     {
+        [Key]
         public int Id { get; set; }
         public string Degree { get; set; }
         public Major Major { get; set; }
--- ECatalogCodingCamp/API/Models/JobRole.cs
+++ ECatalogCodingCamp/API/Models/JobRole.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace API.Models
     [Table("TB_M_JobRole")]
     public class JobRole
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<User> Users { get; set; }

c3c5bd169596ed69218b16fa681d36b1dbb179bb
Arsya Baharin <alfatehanarsya8998@gmail.com>
2021-06-09 19:43:45 +0700

    Rejected with Email

--- ECatalogCodingCamp/API/Controllers/BooksController.cs
+++ ECatalogCodingCamp/API/Controllers/BooksController.cs
@@ -1,4 +1,6 @@
 ﻿using API.Base;
+using API.Context;
+using API.Handlers;
 using API.Models;
 using API.Repositories.Data;
 using API.Repositories.Interface;
@@ -25,11 +27,13 @@ namespace API.Controllers
         private readonly BookRepository bookRepository;
         private readonly IGenericDapper _dapper;
         private readonly IConfiguration config;
-        public BooksController(BookRepository bookRepository, IGenericDapper _dapper, IConfiguration config) : base(bookRepository)
+        private readonly MyContext context;
+        public BooksController(BookRepository bookRepository, IGenericDapper _dapper, IConfiguration config, MyContext context) : base(bookRepository)
         {
             this.bookRepository = bookRepository;
             this._dapper = _dapper;
             this.config = config;
+            this.context = context;
         }
 
         [HttpGet("GetClient/{id}")]
@@ -84,6 +88,31 @@ namespace API.Controllers
             paramsGetClient.Add("Id", id, DbType.Int32);
             return db.Query<dynamic>("[dbo].[SP_GetCandidateInterview]", paramsGetClient, commandType: CommandType.StoredProcedure);
 
+        }
+
+        [HttpDelete("Rejected/{id}")]
+        public IActionResult Rejected(int id)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var readToken = tokenHandler.ReadJwtToken(Request.Headers["Authorization"].ToString().Replace("Bearer ", string.Empty));
+            var emailClient = readToken.Claims.First(getEmail => getEmail.Type == "email").Value;
+
+            var foundClient = context.Users.Where(user => user.Email == emailClient).FirstOrDefault();
+            var nameClient = foundClient.Name;
+
+            var foundCandidate = context.Users.Where(user => user.Id == id).FirstOrDefault();
+            var candidateName = foundCandidate.Name;
+            var candidateEmail = foundCandidate.Email;
+            var message = "Thank you for your time, but your not suitable for this job";
+
+            var paramsGetClient = new DynamicParameters();
+            using IDbConnection db = new SqlConnection(config.GetConnectionString("MyConnection"));
+            paramsGetClient.Add("Id", id, DbType.Int32);
+            var result =  db.Query<dynamic>("[dbo].[SP_Rejected]", paramsGetClient, commandType: CommandType.StoredProcedure);
+
+            var sendEmail = new SendEmail(context);
+            sendEmail.SendRejected(candidateEmail, nameClient, candidateName, message);
+            return Ok(new { result = result, message = "Candidate Has Been Rejected" });
         }
     }
 }
